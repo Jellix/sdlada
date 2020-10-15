cbb324835e3a2cc3788866b7e073bfb010a41254
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-10-15 21:21:36 +0200

    Make use of user data passed to callback instead of using global variables.

--- test/audio/audiostream_callbacks.adb
+++ test/audio/audiostream_callbacks.adb
@@ -11,20 +11,20 @@ package body Audiostream_Callbacks is
    Frame_Size : constant Byte_Count :=
      Stereo_Buffers.Frames'Component_Size / System.Storage_Unit;
 
-   procedure User_Callback (Userdata   : in out User_Data;
-                            Audio_Data : in out Stereo_Buffers.Frames)
+   procedure User_Callback (Input_Stream : in out SDL.Audio.Streams.Audio_Stream;
+                            Audio_Data   : in out Stereo_Buffers.Frames)
    is
       --  FIXME: Proper interface for such usage, i.e. type conversion
       --         between the generic frame type and Buffer_Type.
       Got : Byte_Count;
-      pragma Unreferenced (Userdata, Got);
       Audio_Buf : constant SDL.Audio.Buffer_Type :=
         SDL.Audio.Buffers.To_Buffer
           (Audio_Buf => Buffer_Base (Audio_Data'Address),
            Audio_Len => Audio_Data'Length * Frame_Size);
 
    begin
-      Streams.Stream_Get (Stream, Audio_Buf, Got);
+      Streams.Stream_Get (Input_Stream, Audio_Buf, Got);
+      pragma Unreferenced (Got); --  FIXME
    end User_Callback;
 
 end Audiostream_Callbacks;
